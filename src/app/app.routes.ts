4ffdaaf3f54c21c04ed3a7d00c4b0d36dd0bb380
PauloHGrande <pauloav2013@gmail.com>
2025-01-22 16:50:18 -0300

    Criação do Layout com angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: NavComponent, children: [
+            {
+                path: 'home',
+                component: HomeComponent
+            }
+        ]
+    }
+];
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { NgOptimizedImage } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [NgOptimizedImage, 
+            RouterOutlet, 
+            HeaderComponent,
+            MatSidenavModule, 
+            MatButtonModule, 
+            MatListModule, 
+            MatIconModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['home'])
+  }
+
+}
